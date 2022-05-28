491608b3c1b18e3d75629fde9703cdce2dd78068
wvxbs <gabriel.ferreira7854@gmail.com>
2022-05-27 21:38:09 -0300

    classe Pessoa adicionada

---
+++ Projeto/SeraphineUAM/src/Model/Pessoa.java
@@ -0,0 +1,48 @@
+package Model;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author wvxbs
+ */
+public class Pessoa {
+    private String Nome, Endereco; 
+    private int Idade;
+
+    public Pessoa() {
+        this.Nome = "";
+        this.Endereco = "";
+        this.Idade = 0;
+    }
+
+    public String getNome() {
+        return Nome;
+    }
+
+    public void setNome(String Nome) {
+        this.Nome = Nome;
+    }
+
+    public String getEndereco() {
+        return Endereco;
+    }
+
+    public void setEndereco(String Endereco) {
+        this.Endereco = Endereco;
+    }
+
+    public int getIdade() {
+        return Idade;
+    }
+
+    public void setIdade(int Idade) {
+        this.Idade = Idade;
+    }
+    
+    
+    
+}
