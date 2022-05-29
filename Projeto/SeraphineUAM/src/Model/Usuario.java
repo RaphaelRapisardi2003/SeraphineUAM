e2af0ae2cbb0781c9686ae301f3a8f6ff07cec19
wvxbs <gabriel.ferreira7854@gmail.com>
2022-05-29 17:52:20 -0300

    Classe usuário atualizada

--- Projeto/SeraphineUAM/src/Model/Usuario.java
+++ Projeto/SeraphineUAM/src/Model/Usuario.java
@@ -2,9 +2,7 @@ package Model;
 
 import java.util.List;
 
-public class Usuario extends Pessoa {
-    private List<Item> listaDeItens;
-    
+public class Usuario extends Pessoa {    
     public Usuario () {
     }   
 }
