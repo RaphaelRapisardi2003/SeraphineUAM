5e3ed6a0e2960cf142386b30cc2172e20551d2c0
wvxbs <gabriel.ferreira7854@gmail.com>
2022-05-29 23:11:51 -0300

    Classes produto e venda finalizadas

--- Projeto/SeraphineUAM/src/Model/Produto.java
+++ Projeto/SeraphineUAM/src/Model/Produto.java
@@ -10,4 +10,30 @@ public class Produto {
         this.QtdEstoque = 0;
         this.Preco = 0;
     }
+
+    public String getDescricao() {
+        return Descricao;
+    }
+
+    public void setDescricao(String Descricao) {
+        this.Descricao = Descricao;
+    }
+
+    public double getQtdEstoque() {
+        return QtdEstoque;
+    }
+
+    public void setQtdEstoque(double QtdEstoque) {
+        this.QtdEstoque = QtdEstoque;
+    }
+
+    public float getPreco() {
+        return Preco;
+    }
+
+    public void setPreco(float Preco) {
+        this.Preco = Preco;
+    }
+    
+    
 }
