08f2af695fff859b46c44f79d6f8a473838863f8
wvxbs <gabriel.ferreira7854@gmail.com>
2022-05-30 21:35:22 -0300

    classe validar login pronta

--- Projeto/SeraphineUAM/src/Service/ValidarLogin.java
+++ Projeto/SeraphineUAM/src/Service/ValidarLogin.java
@@ -1,5 +1,17 @@
 package Service;
 
 public class ValidarLogin {
-    
+    public boolean ValidarSenha(String Senha) {
+        if (Senha == null) return false;
+        if (Senha.length() < 8) return false;
+
+        return true;
+    }   
+
+    public boolean ValidarNomeDeUsuario(String NomeDeUsuario) {
+        if (NomeDeUsuario == null) return false;
+        if (NomeDeUsuario.length() < 5) return false;
+
+        return true;
+    }
 }
