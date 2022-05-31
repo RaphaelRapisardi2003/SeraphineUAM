63b1b62e9f889baf761b4c34aedbd513115cfe1a
wvxbs <gabriel.ferreira7854@gmail.com>
2022-05-31 18:21:59 -0300

    .

--- Projeto/SeraphineUAM/src/Controller/TelaLoginController.java
+++ Projeto/SeraphineUAM/src/Controller/TelaLoginController.java
@@ -7,14 +7,14 @@ public class TelaLoginController {
     public boolean EfetuarLogin (Pessoa pessoa) {
         ValidarLogin validarLogin = new ValidarLogin();
 
-        if (!validarLogin.ValidarNomeDeUsuario(pessoa.getNomeDeUsuario())) return false;
-        
-        if (!validarLogin.ValidarSenha(pessoa.getSenha())) return false;
+       if (!validarLogin.ValidarNomeDeUsuario(pessoa.getNomeDeUsuario())) return false;
+       
+       if (!validarLogin.ValidarSenha(pessoa.getSenha())) return false;
 
         return Login(pessoa);
     }
 
     private boolean Login (Pessoa pessoa) {
-        return false;
+        return true;
     }
 }
