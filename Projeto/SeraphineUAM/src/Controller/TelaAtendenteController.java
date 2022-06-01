c7fc527b67d9fe4a54d9d757567ee92f382e5277
wvxbs <gabriel.ferreira7854@gmail.com>
2022-05-31 22:39:50 -0300

    Classe Tela atendente controller atualizada

--- Projeto/SeraphineUAM/src/Controller/TelaAtendenteController.java
+++ Projeto/SeraphineUAM/src/Controller/TelaAtendenteController.java
@@ -1,5 +1,18 @@
 package Controller;
 
+import Model.Produto;
+
 public class TelaAtendenteController {
-    
+    public Object[] AdicionarItem(Produto produto, double QtdCompra){
+        int Id = produto.getId();
+        double QtdEstoque = produto.getQtdEstoque();
+        float Preco = produto.getPreco();
+        float ValorTotal = CalcularValorTotalDoItem(QtdCompra, Preco);
+
+        return new Object[]{ produto.getId(), produto.getQtdEstoque(), produto.getPreco() };
+    }
+
+    private float CalcularValorTotalDoItem(double QtdCompra, float Preco) {
+        return (Preco * (float) QtdCompra);
+    }
 }
