7b9939dacee1d8fc09a7f86c4b7fb8d4b1c7ade2
wvxbs <gabriel.ferreira7854@gmail.com>
2022-05-31 22:39:19 -0300

    Classe produto atualizada

--- Projeto/SeraphineUAM/src/Model/Produto.java
+++ Projeto/SeraphineUAM/src/Model/Produto.java
@@ -1,14 +1,24 @@
 package Model;
 
 public class Produto {
+    private int Id;
     private String Descricao;
     private double QtdEstoque;
     private float Preco;
     
     public Produto() {
+        this.Id = 0;
         this.Descricao = "";
         this.QtdEstoque = 0;
         this.Preco = 0;
+    }
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int id) {
+        Id = id;
     }
 
     public String getDescricao() {
