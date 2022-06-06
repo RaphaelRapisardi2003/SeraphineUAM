7737824c9d1392d52c92113107edab7332f71815
Gabriel Ferreira <gabriel.ferreira7854@gmail.com>
2022-06-06 19:34:29 -0300

    comentários desnecessários removidos

--- Projeto/SeraphineUAM/src/Controller/TelaADMController.java
+++ Projeto/SeraphineUAM/src/Controller/TelaADMController.java
@@ -1,17 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Controller;
 
 import Model.Produto;
 import Model.Produto;
 import javax.swing.*;
 
-/**
- *
- * @author rapha
- */
 public class    TelaADMController {
     private String codigoDeErro;
     
